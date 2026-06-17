6ca54aa1a02b84dc8bed5a9d19cd0177d28685b1
Wilder Taype <i201418741@cibertec.edu.pe>
2026-06-17 02:59:06 -0500

    Actualizacion Principal v12.10.10

--- src/wii.js
+++ src/wii.js
@@ -9,16 +9,16 @@ export let linkweb = 'https://workwii.vercel.app'; // Sin slash (/), al final
 export let lanzamiento = 2026;
 export let by = '@wilder.taype';
 export let linkme = 'https://wtaype.github.io/';
-export let version = 'v11';
+export let version = 'v12';
 
 /** ACTUALIZAR AL TAG POR SEGURIDAD [TAG NUEVO] (1)
-git tag v11 -m "Version v11" ; git push origin v11
+git tag v12 -m "Version v12" ; git push origin v12
 
 ACTUALIZACIÓN AL MAIN PRINCIPAL DEL PROYECTO [MAIN] (2)
-git add . ; git commit -m "Actualizacion Principal v11.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v12.10.10" ; git push origin main
 
 // REEMPLAZAR TAG DE SEGURIDAD EXISTENTE [TAG REMPLAZO] (3)
-git tag -d v11 ; git tag v11 -m "Version v11 actualizada" ; git push origin v11 --force
+git tag -d v12 ; git tag v12 -m "Version v12 actualizada" ; git push origin v12 --force
 
 // Actualizar versiones de seguridad [ELIMINAR CARPETA - ARCHIVO ONLINE] (5)
 git rm --cached skills-lock.json ; git commit -m "Archivo Eliminado" ; git push origin main
